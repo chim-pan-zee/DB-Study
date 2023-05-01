d19d0c5046682852055ccc525969823078ae8012
chim-pan-zee <127918752+chim-pan-zee@users.noreply.github.com>
2023-05-02 08:45:54 +0900

    Update search.php

--- search.php
+++ search.php
@@ -22,32 +22,50 @@
 
 <?php
 $host = 'localhost';
-$user = 'man';
+$user = 'viewer';
 $pass = 'password';
 $db = 'schoolpe';
-$conn = mysqli_connect($host, $user, $pass, $db);
+$connect = mysqli_connect($host, $user, $pass, $db);
 
 $search_term = $_GET['search_term'];
 $search_field = $_GET['search_field'];
 
 $sql = "SELECT * FROM students WHERE $search_field LIKE '%$search_term%'";
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($connect, $sql);
 
-echo "<table>";
-echo "<thead><tr><th>ID</th><th>학번</th><th>이름</th><th>키</th><th>몸무게</th><th>성별</th></tr></thead>";
-echo "<tbody>";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['students_id'] . "</td>";
-    echo "<td>" . $row['students_number'] . "</td>";
-    echo "<td>" . $row['students_name'] . "</td>";
-    echo "<td>" . $row['students_tall'] . "</td>";
-    echo "<td>" . $row['students_weight'] . "</td>";
-    echo "<td>" . $row['students_gender'] . "</td>";
-    echo "</tr>";
+if (!isset($_GET['search_term']) || empty($_GET['search_term'])) {
+  echo "검색어를 입력하세요.";
+  exit;
+} 
+
+if (!$result) {
+  echo "SQL 구문에 오류가 있습니다.";
+  exit;
+} 
+
+if (!$connect) {
+  die("데이터베이스 연결 실패: " . mysqli_connect_error());
 }
-echo "</tbody>";
-echo "</table>";
 
-mysqli_close($conn);
+if (mysqli_num_rows($result) == 0) {
+  echo "검색 결과가 없습니다.";
+} else {
+  echo "<table>";
+  echo "<thead><tr><th>ID</th><th>학번</th><th>이름</th><th>키</th><th>몸무게</th><th>성별</th></tr></thead>";
+  echo "<tbody>";
+  while ($row = mysqli_fetch_assoc($result)) {
+      echo "<tr>";
+      echo "<td>" . $row['students_id'] . "</td>";
+      echo "<td>" . $row['students_number'] . "</td>";
+      echo "<td>" . $row['students_name'] . "</td>";
+      echo "<td>" . $row['students_tall'] . "</td>";
+      echo "<td>" . $row['students_weight'] . "</td>";
+      echo "<td>" . $row['students_gender'] . "</td>";
+      echo "</tr>";
+  }
+  echo "</tbody>";
+  echo "</table>";
+}
+
+mysqli_close($connect);
 ?>
